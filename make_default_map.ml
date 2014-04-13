10775abe10938669dbb2d252140151934ce8eac5
vivien <vivienkro@live.fr>
2014-04-13 14:36:34 +0200

    Ajout du numéro 1, rue Brige

--- make_default_map.ml
+++ make_default_map.ml
@@ -8,11 +8,18 @@ let impasse =
     description = "Vous vous trouvez dans la fameuse impasse des Savates jaunes. \nAu nord se trouve la rue du majoral F. Brige." ;
   };;
 
-let rue =
+let rue_3 =
   { 
     numero_unique = 1 ;
     nom = "Rue du majoral F. Brige (3)" ;
-    description = "Vous vous trouvez dans la non moins fameuse rue du majoral Brige. \nAu sud, se trouve l'impasse des Savates jaunes.\nÀ l'est, la rue continue (4), mais un molosse rébarbatif vous dévisage avec de la bave aux lèvres.\nÀ l'ouest, la rue continue (2), mais un groupe de personnes armées semble mener des transactions, nécessitant le moins de témoins possibles. Ne vous approchez pas !"
+    description = "Vous vous trouvez dans la rue du majoral Brige, devant le numéro 3.\nAu sud, se trouve l'impasse des Savates jaunes.\nÀ l'est, la rue continue (5), mais un molosse rébarbatif vous dévisage avec de la bave aux lèvres.\nÀ l'ouest, la rue continue (1).\nAu nord se trouve le Café Jaune. Fermé à cette heure-ci."
+  } ;;
+
+let rue_1 =
+  {
+    numero_unique = 2 ;
+    nom = "Rue du majoral F. Brige (1)" ;
+    description = "Vous vous trouvez dans la rue du majoral Félix Brige, devant le numéro 1.\nAu sud, le numéro 2.\nAu nord, le numéro 1.\nÀ l'ouest, le boulevard J. Boudin, mais l'accès y est interdit.\nÀ l'est, la rue F. Brige, numéro 3." 
   } ;;
 
 let entree_impasse = 
@@ -27,11 +34,25 @@ let sortie_impasse =
     delai = 1.0
   } ;;
 
-let structure = creer_nuage [|0 ; 1|] ;;
-let zones = [|impasse ; rue|] ;;
-let portes = [|entree_impasse ; sortie_impasse|] ;;
+let rue_31 =
+  {
+    invocation = "ouest" ;
+    delai = 1.0
+  } ;;
+
+let rue_13 =
+  {
+    invocation = "est" ;
+    delai = 1.0 
+  } ;;
+
+let structure = creer_nuage [|0 ; 1 ; 2|] ;;
+let zones = [|impasse ; rue_3 ; rue_1|] ;;
+let portes = [|entree_impasse ; sortie_impasse ; rue_31 ; rue_13|] ;;
 relier structure 0 1 1 ;;
 relier structure 1 0 0 ;;
+relier structure 1 2 2 ;;
+relier structure 2 1 3 ;;
 
 let carte =
 {
